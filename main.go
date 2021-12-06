eb5d29455bb89344233f16952d085dc37d272f27
mir-dhaka <mir.cuet@gmail.com>
2021-12-06 08:28:53 +0600

    Update main.go

--- main.go
+++ main.go
@@ -14,16 +14,18 @@ func getId(seed int, pre string) func() string {
 }
 
 func main() {
-	/* nextId is now a function with i as 0 */
+	/* employeeId is now a function with i as 0 */
 	employeeId := getId(0, "E")
 
-	/* invoke nextNumber to increase i by 1 and return the same */
+	/* invoke employeeId to increase id by 1 and return it with added prefix */
 	fmt.Println(employeeId())
 	fmt.Println(employeeId())
 	fmt.Println(employeeId())
 
-	/* create a new id and see the result, i is the given seed*/
+	/* create id sequence for attendance*/
 	attendanceId1 := getId(10, "A")
+	
+	/*some other calls*/
 	fmt.Println(attendanceId1())
 	fmt.Println(attendanceId1())
 	fmt.Println(employeeId())
